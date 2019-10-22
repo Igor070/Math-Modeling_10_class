e6691f33db062dec03a4144eafa953e266aaf604
Igor070 <56308674+Igor070@users.noreply.github.com>
2019-10-22 17:49:52 +0200

    2222

--- 2 задание.py
+++ 2 задание.py
@@ -1,5 +1,8 @@
-a=1
-b=2
-for i in range(0,10,1):
-    a=a*b
-    print(a)
+from constant_modul import  G
+from math import cos,pi
+h=100 
+b=30
+a=pi/3
+u=((G*h*cos(b)**2)/2*cos(a)**2*(1-cos(b)*cos(a)))**0.5
+print(u)
+print(G)
